5c70e12c46a26380957951b1644e34a441662686
Ashutosh777777 <piprodeashutosh@gmail.com>
2026-07-08 12:53:39 +0530

    Search in a 2D matrix - 2

---
+++ Binary Search/matrix_search_2.py
@@ -0,0 +1,15 @@
+        m = len(matrix)
+        n = len(matrix[0])
+
+        r = 0
+        c = n - 1
+
+        while r < m and c >= 0:
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] > target:
+                c -= 1
+            else:
+                r += 1
+
+        return False
